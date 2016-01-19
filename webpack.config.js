4e2dfb88068cf6ac20590a6945f61000759f6157
Naomi Aro <naomiaro@gmail.com>
2016-01-19 13:44:00 -0800

    Extracting peak library out of waveform-playlist.

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require("webpack");
+var createVariants = require('parallel-webpack').createVariants;
+
+function createConfig(options) {
+  var plugins = [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ];
+  if (options.minified) {
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      compress: {
+        warnings: false
+      }
+    }));
+  }
+
+  return {
+    entry: __dirname + "/src/index.js",
+    output: {
+      path:  __dirname + "/dist/js",
+      filename: 'webaudio-peaks.' +
+        options.target +
+        (options.minified ? '.min' : '')
+        + '.js',
+      library: 'webaudioPeaks',
+      libraryTarget: options.target
+    },
+    plugins: plugins
+  };
+}
+
+module.exports = createVariants({
+  minified: [true, false],
+  target: ['var', 'commonjs2', 'umd', 'amd']
+}, createConfig);
